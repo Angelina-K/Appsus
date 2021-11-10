bf0e92e423d6176df40905137e60c87daa090b40
daniieldvir <91826695+daniieldvir@users.noreply.github.com>
2021-11-10 12:27:34 +0200

    routes

---
+++ js/keep-app/pages/keep-app.cmp.js
@@ -0,0 +1,8 @@
+export default {
+  name: "keep-app",
+  template: `
+          <section class="keep main-layout ">
+              <h1>keep</h1>
+          </section>
+              `,
+};
---
+++ js/mail-app/pages/mail-app.cmp.js
@@ -0,0 +1,8 @@
+export default {
+  name: "mail-app",
+  template: `
+          <section class="keep main-layout ">
+              <h1>mail</h1>
+          </section>
+              `,
+};
