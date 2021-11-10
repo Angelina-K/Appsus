388732f5700835228d44745b94ae6c11867cf505
Angelina-K <79867604+Angelina-K@users.noreply.github.com>
2021-11-10 20:41:10 +0200

    css for read

--- js/apps/mail-app/mail-cmps/mail-list.cmp.js
+++ js/apps/mail-app/mail-cmps/mail-list.cmp.js
@@ -1,18 +1,27 @@
 import mailPreview from "./mail-preview.cmp.js";
+import titleDisplay from "./titleForDisplay.cmp.js";
 export default {
   name: "mail-list",
-  props: ["emails"],
+  props: ["emails", "filterBy"],
   template: `
+  <section class="mail-list">
+  <titleDisplay :title="filterBy"/>
     <ul class="clean-list">
-      <li v-for="email in emails" :key="email.id">
-          <router-link :to="'/mail/'+email.id" >
-            <mailPreview :email="email"/>
-          </router-link>
+      <li v-for="email in emails" :class="{read: email.isRead }" :key="email.id">
+          <!-- <router-link :to="'/mail/'+email.id" > -->
+            <mailPreview :email="email" @click.native="emailClicked(email.id)"/>
+          <!-- </router-link> -->
         </li>
     </ul>
+    </section>
 `,
-
+  methods: {
+    emailClicked(emailId) {
+      this.$router.push("/mail/" + emailId);
+    },
+  },
   components: {
     mailPreview,
+    titleDisplay,
   },
 };
