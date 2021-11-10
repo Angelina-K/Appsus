5dc6abdae2dc705e900dc93805215947b560823f
Angelina-K <79867604+Angelina-K@users.noreply.github.com>
2021-11-10 13:19:38 +0200

    mail-service

--- js/mail-app/pages/mail-app.cmp.js
+++ js/mail-app/pages/mail-app.cmp.js
@@ -1,8 +1,9 @@
+import { bookService } from "../services/mail-servies.js";
 export default {
   name: "mail-app",
   template: `
-          <section class="keep main-layout ">
-              <h1>mail</h1>
-          </section>
-              `,
+    <section class="keep main-layout ">
+        <h1>mail</h1>
+    </section>
+`,
 };
--- js/mail-app/services/mail-servies.js
+++ js/mail-app/services/mail-servies.js
@@ -1,21 +1,63 @@
+import { storageService } from "../../services/async-storage-service.js";
+import { utilService } from "../../services/util-service.js";
+
+export const bookService = {
+  // query,
+  // remove,
+  // save,
+  // getById,
+  // addReview
+};
+
+const EMAIL_KEY = "emails";
+const gEmails = createEmails();
+
+console.log(gEmails);
+
 const loggedinUser = {
-  email: 'user@appsus.com',
-  fullname: 'Mahatma Appsus',
+  email: "user@appsus.com",
+  fullname: "Mahatma Appsus",
 };
 
 const email = {
-  id: 'e101',
-  subject: 'Miss you!',
-  body: 'Would love to catch up sometimes',
+  id: "e101",
+  subject: "Miss you!",
+  body: "Would love to catch up sometimes",
   isRead: false,
   sentAt: 1551133930594,
-  to: 'momo@momo.com',
+  to: "momo@momo.com",
 };
 
 const criteria = {
-  status: 'inbox/sent/trash/draft',
-  txt: 'puki', // no need to support complex text search
+  status: "inbox/sent/trash/draft",
+  txt: "puki", // no need to support complex text search
   isRead: true, // (optional property, if missing: show all)
   isStared: true, // (optional property, if missing: show all)
-  lables: ['important', 'romantic'], // has any of the labels
+  lables: ["important", "romantic"], // has any of the labels
 };
+
+function createEmails() {
+  let emails = utilService.loadFromStorage(EMAIL_KEY);
+  if (!emails || !emails.length) {
+    emails = [
+      _createEmail("work", "do some work", "adi"),
+      _createEmail("something", "do something", "dani"),
+      _createEmail("subject", "nice body", "ggg"),
+      _createEmail("no subject", "whatever", "someone"),
+    ];
+  }
+  utilService.saveToStorage(EMAIL_KEY, emails);
+  return emails;
+}
+
+function _createEmail(subject, body, sendTo) {
+  const email = {
+    id: utilService.makeId(),
+    subject,
+    body,
+    isRead: false,
+    sentAt: new Date().toLocaleString(),
+    to: `${sendTo}@momo.com`,
+  };
+  return email;
+}
